42dfbc313ef388f334e74cf3b9aaa3360830adc5
Biswarup <biswa.ds.ml@gmail.com>
2020-09-24 14:49:26 +0530

    Member Microservice Added

---
+++ membermicroservicetest/membermpremiumtest.cs
@@ -0,0 +1,70 @@
+using membermicroservice.Controllers;
+using membermicroservice.Models;
+using membermicroservice.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace membermicroservicetest
+{
+    public class Tests
+    {
+        List<memberpremium> premiumList = new List<memberpremium>();
+        [SetUp]
+        public void Setup()
+        {
+            premiumList = new List<memberpremium>
+            {
+                 new memberpremium
+                 {
+                    memberid=1,
+                    topup=1000,
+                    premium=2000,
+                    paiddate=DateTime.Today
+                 },
+                 new memberpremium
+                 {
+                    memberid=2,
+                    topup=1000,
+                    premium=2000,
+                    paiddate=DateTime.Today
+                 },
+                 new memberpremium
+                 {
+                    memberid=3,
+                    topup=2000,
+                    premium=3000,
+                    paiddate=DateTime.Today
+                 },
+                 new memberpremium
+                 {
+                    memberid=4,
+                    topup=4000,
+                    premium=5000,
+                    paiddate=DateTime.Today
+                 },
+                  
+            };
+            
+
+        }
+
+        [Test]
+        public void GetAllTest()
+        {
+            Mock<memberpremiumrepo> mock = new Mock<memberpremiumrepo>();
+            mock.Setup(p => p.fun()).Returns(premiumList);
+            billsController controller = new billsController();
+            IEnumerable<memberpremium> result = controller.Get();
+            Assert.AreEqual(4, result.Count());
+            
+
+
+
+
+        }
+    }
+}
